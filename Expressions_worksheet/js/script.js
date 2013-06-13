8df49ff47d944d1d62e8ce5703d01ca0d409a6f5
katybrunette <katybrunette@fullsail.edu>
2013-06-12 23:08:04 -0500

    This is my submission of the Expressions Worksheet assignment.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,59 @@
+//Katy Brunette - Expressions Worksheet - June 12th, 2013
+
+//Problem One
+var humanAge = 5; 
+var dogAge = 35; 
+var seven = 7; 
+
+dogAge = humanAge * seven; 
+console.log(dogAge);
+
+
+//Problem Two
+var partyGoers = 8; 
+var pizzaSlices = 6; 
+var pizzaOrders = 7; 
+
+var pizzaEach = pizzaSlices * pizzaOrders / partyGoers;
+console.log(pizzaEach);
+
+
+//Problem Three
+
+var partyGuests = 8; 
+var pizzas = 6;
+var orders = 7;  
+var total = 42; 
+
+var remainder =  total % partyGuests; 
+console.log(remainder); 
+
+
+//Problem Four
+
+var weekTotal = [84,52,38,67,45];
+
+var average = (weekTotal[0] + weekTotal[1] + weekTotal[2] + weekTotal[3] + weekTotal[4]) / 5;
+console.log(average);
+
+
+//Problem Five
+
+var regPrice = 50;
+var discPer = 15; 
+var item = "Jacket";
+var salesTax = 5; 
+
+var discount = .15 * regPrice;
+console.log(discount);
+
+var withoutTax = regPrice - discount; 
+console.log(withoutTax);
+
+var withTax = withoutTax + 2.125;
+console.log(withTax);
+
+
+
+
+
