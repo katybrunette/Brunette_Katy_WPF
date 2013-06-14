2e3cbff46f46b708ae29f7e334c4b0d5bb306ff2
katybrunette <katybrunette@fullsail.edu>
2013-06-13 22:23:55 -0500

    Added more information to the comments for reference.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,4 +2,7 @@
 //What is the average number of calories that I burn in a week from pole dancing?
 
 var oneHour = "420 calories";//one hour of pole dancing burns 420 calories
+//array = 5 different calorie amounts for 5 days of the week
+
+
 
