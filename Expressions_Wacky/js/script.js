f5e5797bf1260ccd2c79ecdcbdc69731404f3402
katybrunette <katybrunette@fullsail.edu>
2013-06-13 22:22:06 -0500

    Added base calorie amount for Wacky expression.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 //WPF - Katy Brunette - Expressions Assignment: Wacky - 6/13/13
 //What is the average number of calories that I burn in a week from pole dancing?
- 
+
+var oneHour = "420 calories";//one hour of pole dancing burns 420 calories
+
