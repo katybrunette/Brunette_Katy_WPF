b177c5fc37f3fa741e4097de2f1b4ae6a70c4917
katybrunette <katybrunette@fullsail.edu>
2013-06-13 22:12:58 -0500

    Added more variables.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,8 @@
 //WPF - Katy Brunette - Expressions Assignment: Industry - 6/13/13
 //How many megabytes are in one gigabyte? 
 
-var megaByte = 1; 
-var gigaByte = 1024; 
+var megaByte = 1024; //base number for one megabytes
+var gigaByte = 1;//showing that just one gigabyte = 1024 megabytes
+
+var gigaByte = 250;//giving a variable to gigaByte for calculation 
 
