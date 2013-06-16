211096e155d1cc1602f45021190c953b5b4746ba
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:13:18 -0500

    Added in comments to provide information on code.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,6 +1,6 @@
 //WPF - Conditionals Assignment - Personal - Katy Brunette - 6/15/13
 
-var saveMoney = 3000; 
-var years = 2; 
+var saveMoney = 3000; //3000 = dollars saved up to be able to go to Paris.
+var years = 2; //I need to have 3000 saved up in 2 years so I can go after I graduate.
 
 
