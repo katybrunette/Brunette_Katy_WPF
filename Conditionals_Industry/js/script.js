d924a4d876a219fb48199d60ad44bbe5ffa44335
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:41:14 -0500

    Changed variable to make the result true. Tested and it prints correctly to the console.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,6 +1,6 @@
 //WPF - Conditionals Assignment - Industry - Katy Brunette - 6/15/13
 
-var print = 200; //print resolution should be 300+. 
+var print = 350; //print resolution should be 300+. 
 var web = 72; //web resolution is always 72.
 
 (print >= 300) ? console.log("You will have a high res print quality file.") : console.log("You will not have a high res print quality file.");
