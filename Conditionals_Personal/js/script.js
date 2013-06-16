044a75870e71d3970a65fe61a4861d8b25446263
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:27:11 -0500

    Finished coding. Tested both results. Both worked.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,11 +3,11 @@
 var saveMoney = 1500; //3000 = dollars saved up to be able to go to Paris.
 var years = 2; //I need to have 3000 saved up in 2 years so I can go after I graduate.
 
-if(saveMoney === 3000){ 
-	console.log("If I can save $" + saveMoney + " in " + years + " years then I can go to Paris!");
+if(saveMoney === 3000){ //if the amount is equal to 3000, I can go to Paris in 2 years.
+	console.log("If I can save $" + saveMoney + " in " + years + " years then I can go to Paris!");//this calculates whether or not I can go.
 
 }else{
-	console.log("Sorry, you have to wait to go to Paris. :(");
+	console.log("Sorry, you have to wait to go to Paris. :(");//end result - prints to the console my result.
 }
 
 
