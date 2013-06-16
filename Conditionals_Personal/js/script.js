6e41866fa6dcb4cbee924229a608211473e80204
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:21:40 -0500

    Something is wrong in my code…it's not printing out the alternative result.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,12 +1,12 @@
 //WPF - Conditionals Assignment - Personal - Katy Brunette - 6/15/13
 
-var saveMoney = 3000; //3000 = dollars saved up to be able to go to Paris.
+var saveMoney = 1500; //3000 = dollars saved up to be able to go to Paris.
 var years = 2; //I need to have 3000 saved up in 2 years so I can go after I graduate.
 
 if(saveMoney = 3000){ 
 	console.log("If I can save $" + saveMoney + " in " + years + " years then I can go to Paris!");
-
-}else{
+}
+else{
 	console.log("Sorry, you have to wait to go to Paris. :(");
 }
 
