03b90bb78cbbe2d4c2f1e41b4ea0fc8aeaa0be5d
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:32:32 -0500

    First draft code for industry. Need to change format to fit qualifications. Just testing code.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,4 +1,11 @@
 //WPF - Conditionals Assignment - Industry - Katy Brunette - 6/15/13
 
-var print; 
+var print = 200; 
 var web; 
+
+if(print >= 300){
+	console.log("You will have a high res, quality file.");
+
+}else{
+	console.log("You will have a web only quality file.");
+}
