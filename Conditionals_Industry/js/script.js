90099f638137113af0f6197c0a39f4fe6a786571
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:35:43 -0500

    Defined other variable. Added comments to some of the code. Still need to put into differnet format.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,7 +1,7 @@
 //WPF - Conditionals Assignment - Industry - Katy Brunette - 6/15/13
 
-var print = 200; 
-var web; 
+var print = 200; //print resolution should be 300+. 
+var web = 72; //web resolution is always 72.
 
 if(print >= 300){
 	console.log("You will have a high res, quality file.");
