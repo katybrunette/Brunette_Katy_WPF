d554d3022e4ceec27a93b7179306eb838ecedf77
katybrunette <katybrunette@fullsail.edu>
2013-06-15 23:01:33 -0500

    After a few changes and tests, I got the "else if" statement to work! Console printed the alternative result correctly.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,14 +8,14 @@ stripping != poleDance; //these are NOT the same
 
 //adding "else if" to this part of the assignment. 
 
-var age = 18; 
-var ageWithConsent = 17;
+var age = 17; 
+var ageWithConsent = 16;
 var painTol = "high"; 
 var money = "enough";
 
 if(age >= 18){
 	console.log("You are old enough to get a tattoo!");
-} else if(ageWithConsent < ageWithConsent){
+} else if(ageWithConsent < age){
 	console.log("Sorry, you can only get a tattoo if your parent/guardian consents.");
 }
 
