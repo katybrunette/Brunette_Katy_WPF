a98a44813b15ca43365105edef71936f1af5561d
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:40:08 -0500

    Changed format to a ternary. Tested and code still worked - which means I transformed the code correctly into ternary format.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,9 +3,4 @@
 var print = 200; //print resolution should be 300+. 
 var web = 72; //web resolution is always 72.
 
-if(print >= 300){
-	console.log("You will have a high res, quality file.");
-
-}else{
-	console.log("You will have a web only quality file.");
-}
+(print >= 300) ? console.log("You will have a high res print quality file.") : console.log("You will not have a high res print quality file.");
