cfa1af609113dce619b0f25ebba1d7f58e869017
katybrunette <katybrunette@fullsail.edu>
2013-06-15 22:58:59 -0500

    Attempted to turn this into an "else if" statement. Need to test and make sure it worked.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,10 +9,13 @@ stripping != poleDance; //these are NOT the same
 //adding "else if" to this part of the assignment. 
 
 var age = 18; 
+var ageWithConsent = 17;
 var painTol = "high"; 
 var money = "enough";
 
 if(age >= 18){
 	console.log("You are old enough to get a tattoo!");
+} else if(ageWithConsent < ageWithConsent){
+	console.log("Sorry, you can only get a tattoo if your parent/guardian consents.");
 }
 
