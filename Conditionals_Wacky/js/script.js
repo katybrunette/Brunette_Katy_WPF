9496b70d13a5424ab147bcabd254a338bab78edb
katybrunette <katybrunette@fullsail.edu>
2013-06-15 23:05:10 -0500

    Everything has been cleaned up and re-tested. Code works. Variables are good. Added comments for every line of code to provide information.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,12 +8,12 @@ stripping != poleDance; //these are NOT the same
 
 //adding "else if" to this part of the assignment. 
 
-var age = 17; 
-var ageWithConsent = 16;
+var age = 17; //age to get a tattoo - needs to be 18 to get without parental consent.
+var ageWithConsent = 16; //can get tattoo with parental consent at this age.
 
-if(age >= 18){
-	console.log("You are old enough to get a tattoo!");
-} else if(ageWithConsent < age){
-	console.log("Sorry, you can only get a tattoo if your parent/guardian consents.");
+if(age >= 18){ //if the age of the person is greater than or equal to 18, they don't need consent. 
+	console.log("You are old enough to get a tattoo!"); //console prints out this statement if the above is true.
+} else if(ageWithConsent < age){ //else if part means if you are younger than 18, you need consent. 
+	console.log("Sorry, you can only get a tattoo if your parent/guardian consents."); //this prints out final result. 
 }
 
