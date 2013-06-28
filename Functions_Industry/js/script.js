014127e661ad2d0011aa9719d1d875e9bef8e01e
katybrunette <katybrunette@fullsail.edu>
2013-06-27 20:09:08 -0500

    Started new function code for industry.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,7 +1,12 @@
 //WPF - Functions: Industry - Katy Brunette - 6/27/2013
 
-function printRes(){
-	alert("Print resolution should be at 300 or higher.");
+function printRes(){ //named my function printRes for print resolution
+	alert("Print resolution should be at 300ppi or higher."); //
 	console.log(printRes);
 }
 printRes();
+
+
+function screenRes(){
+	
+}
