f3da559243db3330c02b708fcc73f8174e0c974e
katybrunette <katybrunette@fullsail.edu>
2013-06-27 21:50:04 -0500

    Forgot to add the comments - added comments.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,9 +1,9 @@
 //WPF - Functions: Personal - Katy Brunette - 6/27/2013
 
-function hoopCalsBurned(){
-	var hours = 2;
-	var calsPerHr = 420;
-	var hoopCalsBurned = hours * calsPerHr;
-	console.log(hoopCalsBurned);
+function hoopCalsBurned(){//function name means calories burned by hooping
+	var hours = 2;//2 hours of hooping
+	var calsPerHr = 420;//you burn 420 calories per hour by hula hooping
+	var hoopCalsBurned = hours * calsPerHr;//calculation to get the value
+	console.log(hoopCalsBurned);//prints to the console the total number of calories burned by hooping
 }
-hoopCalsBurned();
+hoopCalsBurned();//calls the hoopCalsBurned function 
