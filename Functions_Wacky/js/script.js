8b6201dbeb0754e044abf954b0457e296b6d5074
katybrunette <katybrunette@fullsail.edu>
2013-06-27 22:16:31 -0500

    Added comments and finished.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,14 +1,16 @@
 //WPF - Functions: Wacky - Katy Brunette - 6/27/2013
 
-var vampDracula = function(){
-	prompt("What do vampires want?");
-	return vampDracula;
+var vampDracula = function(){ //declared variable vampDracula for an anonymous function
+	prompt("What do vampires want?"); //created a prompt asking user a question
+	return vampDracula; //added in a return
 }
-vampDracula();
+vampDracula(); //called the function
 
-function vampText(){
-	var vampFood = "blood"; 
-	var vamp = "Dracula";
-	console.log(vamp + " wants to drink your " + vampFood);
+
+
+function vampText(){ //created new function
+	var vampFood = "blood"; //defined first variable
+	var vamp = "Dracula"; //defined second variable
+	console.log(vamp + " wants to drink your " + vampFood); //printed to the console
 }
-vampText();
+vampText(); //called the function 
