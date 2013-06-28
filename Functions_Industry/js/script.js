2eeec8c351ea7fd88d1f45f1f4758883decde52a
katybrunette <katybrunette@fullsail.edu>
2013-06-27 20:58:08 -0500

    Finished testing code.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,12 +1,7 @@
 //WPF - Functions: Industry - Katy Brunette - 6/27/2013
 
 function printRes(){ //named my function printRes for print resolution
-	alert("Print resolution should be at 300ppi or higher."); //
-	console.log(printRes);
+	alert("Print resolution should be at 300ppi or higher."); //text the alert box will display
+	console.log(printRes);//print to console
 }
-printRes();
-
-
-var a = function pixelCount(){
- 	a -- ; 
-}
+printRes();//this calls the function
