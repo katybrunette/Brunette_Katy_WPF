51abf2344590bc2847d1c79bdc201ae08d322f1b
katybrunette <katybrunette@fullsail.edu>
2013-06-27 22:27:27 -0500

    Completed and tested problem one. Just need to add comments, then can start problem two.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,1 +1,13 @@
 //WPF - Functions Worksheet - Katy Brunette - 6/27/2013
+
+//Problem One
+function circ(){
+	var radius = 12;
+	var pi = 3.14;  
+	var circ = pi * radius * 2; 
+	console.log("The circumference of the circle is " + circ);
+}
+circ();
+
+
+//Problem Two
